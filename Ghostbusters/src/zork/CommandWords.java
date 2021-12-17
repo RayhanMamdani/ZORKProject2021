63c8d7eaa95905c5f5f34927d2f6bd12770b85f0
Brandon Tai <btai@bayviewglen.ca>
2021-12-16 21:37:49 -0500

    Picking up items and inventory finished!!!

--- Ghostbusters/src/zork/CommandWords.java
+++ Ghostbusters/src/zork/CommandWords.java
@@ -3,7 +3,7 @@ package zork;
 public class CommandWords {
   // a constant array that holds all valid command words
   private static final String validCommands[] = { "north", "east", "south", "west", "quit", "look", "take", "open",
-      "close", "drop", "unlock", "cut", "down", "jump", "shoot", "use", "go","drive"};
+      "close", "drop", "unlock", "cut", "down", "jump", "shoot", "use", "go","drive","inventory"};
 
   /**
    * Check whether a given String is a valid command word. Return true if it is,
